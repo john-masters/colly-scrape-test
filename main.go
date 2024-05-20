315562230272ab8c60e3611d3fd7edd25f133eb4
John Masters <mastersjohnr@gmail.com>
2024-05-20 18:11:39 +1000

    feat: add colly and colly sample

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"log"
+
+	"github.com/gocolly/colly"
 )
 
 func main() {
-	log.Println("Hello World")
+	c := colly.NewCollector()
+
+	// Find and visit all links
+	c.OnHTML("a", func(e *colly.HTMLElement) {
+		e.Request.Visit(e.Attr("href"))
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		log.Println("Visiting", r.URL)
+	})
+
+	c.Visit("http://go-colly.org/")
 }
