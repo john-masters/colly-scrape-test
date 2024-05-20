c0de21557ed3c18df3e29e9cfdb6caf7ef84c6c0
John Masters <mastersjohnr@gmail.com>
2024-05-20 20:05:49 +1000

    feat: scraping past days jobs

--- main.go
+++ main.go
@@ -6,17 +6,55 @@ import (
 	"github.com/gocolly/colly"
 )
 
+type Job struct {
+	Title       string
+	Company     string
+	Link        string
+	Description string
+}
+
 func main() {
+	var jobs []Job
+
 	c := colly.NewCollector()
 
-	// Find and visit all links
-	c.OnHTML("a", func(e *colly.HTMLElement) {
-		e.Request.Visit(e.Attr("href"))
+	c.OnHTML("[data-card-type='JobCard']", func(e *colly.HTMLElement) {
+		title := e.ChildText("a[data-automation='jobTitle']")
+		company := e.ChildText("a[data-automation='jobCompany']")
+		link := e.ChildAttr("a[data-automation='jobTitle']", "href")
+
+		fullLink := "https://www.seek.com.au" + link
+
+		job := Job{
+			Title:   title,
+			Company: company,
+			Link:    fullLink,
+		}
+
+		jobs = append(jobs, job)
+
+		e.Request.Visit(fullLink)
 	})
 
-	c.OnRequest(func(r *colly.Request) {
-		log.Println("Visiting", r.URL)
+	c.OnHTML("div[data-automation='jobAdDetails']", func(e *colly.HTMLElement) {
+		description := e.Text
+
+		for i := range jobs {
+			if jobs[i].Link == e.Request.URL.String() {
+				jobs[i].Description = description
+				break
+			}
+		}
 	})
 
-	c.Visit("http://go-colly.org/")
+	c.Visit("https://www.seek.com.au/junior-developer-jobs/full-time?daterange=1")
+
+	log.Println("Jobs found: ", len(jobs))
+
+	for _, job := range jobs {
+		log.Println("Title: ", job.Title)
+		log.Println("Company: ", job.Company)
+		log.Println("Link: ", job.Link)
+		log.Println("Description: ", job.Description)
+	}
 }
